88af40f96d438ffb7759986c752db59849ee7aaa
mattiapomelli <mattiapomelli@gmail.com>
2020-09-15 17:25:34 +0200

    dark mode switch

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,42 @@ import World from './components/World'
 import CountryCard from './components/CountryCard'
 import Statistics from './components/Statistics'
 import Searchbar from './components/Searchbar'
+import './css/theme.css'
 import "./style.css"
 import { WorldContext } from './context/WorldContext';
 
 function App() {
   const { selected } = useContext(WorldContext)
 
+  const toggleTheme = (event) => {
+    if(event.target.checked) {
+      transition()
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+      transition()
+      document.documentElement.setAttribute('data-theme', 'light')
+    }
+  }
+
+  const transition = () => {
+    document.documentElement.classList.add('transition')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('transition')
+    }, 1000)
+  }
+
   return (
     <div className="App">
+  
+    <input type="checkbox" id="switch" name="theme" onChange={toggleTheme}/><label htmlFor="switch">Toggle</label>
+
       <div className="main-container">
         <World />
         <div className="country-card">
           {
             selected ? <CountryCard /> :
             <div className="empty-card-container">
-              <h4>Click on a country</h4>
+              <h4 className="try">Click on a country</h4>
             </div>
           }
         </div>
