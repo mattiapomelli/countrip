369773b3c5bdc4807e5e84d4112aaeffd4250ebc
mattiapomelli <mattiapomelli@gmail.com>
2020-09-16 08:24:11 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,19 @@ import World from './components/World'
 import CountryCard from './components/CountryCard'
 import Statistics from './components/Statistics'
 import Searchbar from './components/Searchbar'
+import Navbar from './components/Navbar'
 import './css/theme.css'
 import "./style.css"
 import { WorldContext } from './context/WorldContext';
-import mapStyles from './utils/mapStyles'
+
 
 function App() {
-  	const { selected, layersRef, activeProperty, activeLayer, setTheme } = useContext(WorldContext)
-
-  	const toggleTheme = (event) => {
-
-    	if(event.target.checked) {
-			changeTheme('dark')
-        
-    	} else {
-			changeTheme('light')
-    	}
-	  }
-	  
-	const changeTheme = (mode) => {
-		themeTransition()
-      	document.documentElement.setAttribute('data-theme', mode)
-      	setTheme(mode)
-      	layersRef.current.leafletElement.setStyle(mapStyles[mode].default)				// change theme of default things
-      	layersRef.current.leafletElement.setStyle(mapStyles[mode][activeProperty])		// for active property
-      	if(activeLayer.current){
-        	activeLayer.current.setStyle(mapStyles[mode].active)						// for active layer
-      	}
-	}
-
-	const themeTransition = () => {			//sets a temporary class for transition all the changes caused by the team
-		document.documentElement.classList.add('transition')
-		window.setTimeout(() => {
-			document.documentElement.classList.remove('transition')
-		}, 1000)
-	}
+  	const { selected } = useContext(WorldContext)
 
   return (
     <div className="App">
-  
-    <input type="checkbox" id="switch" name="theme" onChange={toggleTheme}/><label htmlFor="switch">Toggle</label>
+	
+	<Navbar />
 
 		<div className="main-container">
 			<World />
@@ -50,13 +23,12 @@ function App() {
 			{
 				selected ? <CountryCard /> :
 				<div className="empty-card-container">
-				<h4 className="try">Click on a country</h4>
+					<h4 className="try">Click on a country</h4>
 				</div>
 			}
 			</div>
 			
 			<Searchbar />
-			{/* <input type="color" onChange={changeColor}/> */}
 			<Statistics />
 		</div>
     </div>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, {useContext} from "react"
+import { WorldContext } from "../context/WorldContext"
+import mapStyles from '../utils/mapStyles'
+import "../css/navbar.css"
+
+const Navbar = () => {
+    const { layersRef, activeProperty, activeLayer, setTheme } = useContext(WorldContext)
+
+    const toggleTheme = (event) => {
+
+    	if(event.target.checked) {
+			changeTheme('dark')
+        
+    	} else {
+			changeTheme('light')
+    	}
+	}
+	  
+	const changeTheme = (mode) => {
+		themeTransition()
+      	document.documentElement.setAttribute('data-theme', mode)
+      	setTheme(mode)
+      	layersRef.current.leafletElement.setStyle(mapStyles[mode].default)				// change theme of default things
+      	layersRef.current.leafletElement.setStyle(mapStyles[mode][activeProperty])		// for active property
+      	if(activeLayer.current){
+        	activeLayer.current.setStyle(mapStyles[mode].active)						// for active layer
+      	}
+	}
+
+	const themeTransition = () => {			//sets a temporary class for transition all the changes caused by the team
+		document.documentElement.classList.add('transition')
+		window.setTimeout(() => {
+			document.documentElement.classList.remove('transition')
+		}, 1000)
+    }
+    
+    return (
+        <div className="header-container">
+            <header>
+                <div className="logo">Countrip</div>
+
+                <input type="checkbox" id="switch" name="theme" onChange={toggleTheme}/><label htmlFor="switch">Toggle</label>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
